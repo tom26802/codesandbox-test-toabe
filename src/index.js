46846d5583f4b0b5c6d5f010da6ca5e2dd0bde24
tom26802 <81498162+tom26802@users.noreply.github.com>
2021-03-31 09:27:11 +0900

    js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグとliタグを生成
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.innerText = inputText;
+
+  // ボタン作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    const addTarget = completeButton.parentNode;
+    console.log(addTarget.innerText);
+    deleteFromImcompleteList(completeButton.parentNode.parentNode);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //　押されたボタンの親タグを削除
+    deleteFromImcompleteList(deleteButton.parentNode.parentNode);
+  });
+  // liタグの下にdivタグを入れる
+  li.appendChild(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("imcomplete-list").appendChild(li);
+};
+
+const deleteFromImcompleteList = (target) => {
+  document.getElementById("imcomplete-list").removeChild(target);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
